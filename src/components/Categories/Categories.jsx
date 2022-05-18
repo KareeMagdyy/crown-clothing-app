5a2b0f392372e46962d482a287572b537ab5feb4
KareeMagdyy <karim67899@gmail.com>
2022-05-18 16:44:00 +0200

    added Categories Component nad updated with category Item component

---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,14 @@
+import CategoryItem from "../Category-item/CategoryItem";
+import "./Categories.styles.scss";
+
+const Categories = ({ categories }) => {
+  return (
+    <div className='categories-container'>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default Categories;
